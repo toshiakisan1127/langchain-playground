d74f23be80db0b3d8a366e37ce34598657f63011
toshiakisan1127 <toshiakisan1127@gmail.com>
2024-04-26 20:13:24 +0000

    会話の履歴を保持するように

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
 
 load_dotenv()
 
@@ -15,8 +17,13 @@ def create_agent_chain():
             temperature = os.environ["OPENAI_API_TEMPERATURE"],
             streaming = True
         )
+    
+    # OpenAI Fuctions AgentのプロンプトにMemoryの会話履歴を追加するための設定
+    agent_kwargs = {"extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")]}
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+    
     tools = load_tools(["ddg-search", "wikipedia"])
-    return initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS)
+    return initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS, agent_kwargs=agent_kwargs, memory=memory)
 
 st.title("langchain練習用のUI")
 
@@ -39,8 +46,9 @@ if prompt:
     # AIのアイコンで固定の応答を用意してマークダウン形式で返す
     with st.chat_message("assistant"):
         callback = StreamlitCallbackHandler(st.container())
-        agent_chain = create_agent_chain()
-        response = agent_chain.invoke(prompt, callbacks=[callback])
+        if "agent_chain" not in st.session_state:
+            st.session_state.agent_chain = create_agent_chain()
+        response = st.session_state.agent_chain.invoke(prompt, callbacks=[callback])
         st.markdown(response["output"])
     st.session_state.messages.append({"role": "assistant", "content": response["output"]})
     
